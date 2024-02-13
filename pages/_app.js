a90d16a2c045c22311c80937f1003e70a4b06bbb
Michael Clark <mc2.dev@proton.me>
2024-02-13 09:59:20 -0600

    remove boilerplate

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
+import { Manrope } from "next/font/google";
+
+const manrope = Manrope({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Michael Clark</title>
+        <meta name="description" content="Full-stack developer" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={manrope.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
