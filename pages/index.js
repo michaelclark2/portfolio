ede8f1565a32eb8f26bb50f6e31a3ab524294ca9
Michael Clark <mc2.dev@proton.me>
2024-02-13 11:27:58 -0600

    setup vercel/postgres

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import { sql } from "@vercel/postgres";
+const { rows } = await sql`select * from projects`;
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-6xl font-bold text-center">Michael Clark</h1>
+      {rows.map((row) => (
+        <p key={row.id}>{row.name}</p>
+      ))}
     </div>
   );
 }
