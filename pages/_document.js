1b834608d616d6ef57c9d07019c2f81b6c013309
Michael Clark <mc2.dev@proton.me>
2024-02-14 10:26:07 -0600

    added about and section components

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-yellow-500">
+      <body className="bg-slate-200">
         <Main />
         <NextScript />
       </body>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import About from "@/components/About";
+import Section from "@/components/Section";
 import prisma from "@/lib/prisma";
 
 export const getServerSideProps = async () => {
@@ -7,29 +9,15 @@ export const getServerSideProps = async () => {
 
 export default function Home({ projects }) {
   return (
-    <div className="flex flex-col space-y-5 w-full sm:w-3/5 mx-auto p-4">
-      <section className="flex flex-row border-4 m-auto rounded-2xl border-black bg-red-800">
-        <div className="w-2/6 sm:p-5 flex justify-center items-center p-2 ">
-          <img
-            src="./profile.jpg"
-            className="rounded-2xl border-4 border-black"
-          />
-        </div>
-        <div className="w-4/6  sm:p-5 flex flex-col p-2">
-          <h1 className="text-[8vw] sm:text-[4.5vw] font-extrabold">
-            Michael Clark
-          </h1>
-          <p className="leading-tight text-semibold sm:text-[2vw]">
-            Full-stack developer with a penchant for creating beautiful,
-            performant products.
-          </p>
-        </div>
-      </section>
-      <section>
+    <div className="flex flex-col space-y-5 w-full max-w-2xl mx-auto p-4">
+      <About />
+      <Section title="Projects" colorClass="bg-purple-800">
         {projects.map((row) => (
           <p key={row.id}>{row.title}</p>
         ))}
-      </section>
+      </Section>
+      <Section title="Skills" colorClass="bg-blue-800" />
+      <Section title="Contact" colorClass="bg-teal-800" />
     </div>
   );
 }
