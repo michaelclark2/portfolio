5b5559ed45e6e85b35f1a58fe455160dcf27a699
Michael Clark <mc2.dev@proton.me>
2024-02-15 15:15:42 -0600

    added shadows to sections

--- components/Section.js
+++ components/Section.js
@@ -2,7 +2,8 @@ export default function Section({ title, colorClass, children }) {
   return (
     <section
       className={
-        "flex flex-col border-black border-4 rounded-2xl px-4 " + colorClass
+        "flex flex-col border-black border-4 rounded-2xl px-4 shadow-lg " +
+        colorClass
       }
     >
       <h2 className="text-4xl font-extrabold py-4">{title}</h2>
