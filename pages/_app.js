bcabd4262e3855372f42d782e501665c92b07313
Michael Clark <mc2.dev@proton.me>
2024-02-15 15:42:38 -0600

    updated favicon

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }) {
         <title>Michael Clark</title>
         <meta name="description" content="Full-stack developer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <main className={manrope.className}>
         <Component {...pageProps} />
