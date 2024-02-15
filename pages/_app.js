c952f9b2925cb80d9a42c40cf1c8a4ea25094d29
Michael Clark <mc2.dev@proton.me>
2024-02-15 17:48:47 -0600

    fix og:image url

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }) {
         <title>Michael Clark</title>
         <meta name="description" content="Full-stack developer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:image" content="./preview.png" />
+        <meta property="og:image" content="/preview.png" />
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main className={manrope.className}>
